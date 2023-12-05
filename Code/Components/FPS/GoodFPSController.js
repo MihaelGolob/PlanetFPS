14c82a7fbb1a0c5defcb41d0d45dc8f39ec80feb
mihael.golob <golob.mihael@gmail.com>
2023-12-05 19:26:39 +0100

    fps controller clean up

--- Code/Components/FPS/GoodFPSController.js
+++ Code/Components/FPS/GoodFPSController.js
@@ -11,24 +11,33 @@ export class GoodFPSController {
     this.body = body;
     this.cam = cam;
 
-    this.angleX = 0;
-    this.grounded = false;
-
-    this.dx = 0;
-    this.dy = 0;
-
-    this.sensitivity = 0.05;
-    this.speed = 8; 
+    // rotate parameters
+    this.sensitivity = 0.08;
     this.gravityAcceleration = 9.8;
 
-    this.gravityVelocity = 0;
+    // movement parameters
+    this.speed = 8; 
+    this.airSpeed = 5;
+    this.jumpHeight = 5;
 
+    this.isGrounded = true;
+
+    // mouse delta
+    this.dx = 0;
+    this.dy = 0;
+    this.angleX = 0;
+    
+    // gravity
+    this.gravityVelocity = 0;
+    this.grounded = false;
+
+    // input
+    this.keysToTrack = ['KeyW', 'KeyA', 'KeyS', 'KeyD', 'Space'];
     this.initInputHandler();
   }
 
   initInputHandler() {
     this.keysDictionary = {};
-    this.keysToTrack = ['KeyW', 'KeyA', 'KeyS', 'KeyD', 'Space'];
     this.setupKeysDictionary();
 
     document.addEventListener('keydown', (event) => {
@@ -69,7 +78,6 @@ export class GoodFPSController {
     });
   }
 
-
   vec3FromVec4(v4) {
     if(v4[3]) {
         return vec3.fromValues(v4[0]/v4[3], v4[1]/v4[3], v4[2]/v4[3]);
@@ -97,59 +105,54 @@ export class GoodFPSController {
     quat.mul(this.body.rotation, rot, this.body.rotation);
   }
 
-updateMovement(dt) {
-  let moveDir = vec4.fromValues(this.keysDictionary['KeyD'] - this.keysDictionary['KeyA'], 0, this.keysDictionary['KeyS'] - this.keysDictionary['KeyW'], 0);
-  if (vec4.len(moveDir) != 0)   
-    vec4.normalize(moveDir, moveDir);
+  updateMovement(dt) {
+    let moveDir = vec4.fromValues(this.keysDictionary['KeyD'] - this.keysDictionary['KeyA'], 0, this.keysDictionary['KeyS'] - this.keysDictionary['KeyW'], 0);
 
-  vec4.scale(moveDir, moveDir, this.speed * dt);
-  
-  let globalMatrix = mat4.create();
-  mat4.mul(globalMatrix, this.root.matrix, this.body.matrix);
-  
-  let result = vec4.create();
-  vec4.transformMat4(result, moveDir, globalMatrix);
-  result = this.vec3FromVec4(result);
-  
-  vec3.add(this.root.translation, this.root.translation, result);
-  
-  // apply gravity
-  let bodyDownDir = this.vec3FromVec4(vec4.transformMat4(vec4.create(), vec4.fromValues(0, -1, 0, 0), globalMatrix));
-  let globalBodyPos = this.vec3FromVec4(vec4.transformMat4(vec4.create(), vec4.fromValues(0, 0, 0, 1), globalMatrix));
-  let gravityDir = vec3.normalize(vec3.create(), globalBodyPos);
-  vec3.scale(gravityDir, gravityDir, -1);
-  let rotation = quat.rotationTo(quat.create(), bodyDownDir, gravityDir);
-  quat.mul(this.root.rotation, rotation, this.root.rotation);
-  
-  let globalRootPos = this.vec3FromVec4(vec4.transformMat4(vec4.create(), vec4.fromValues(0, 0, 0, 1), this.root.matrix));
-  let isGrounded = vec3.len(globalRootPos) <= 10;
+    if (vec4.len(moveDir) != 0)   
+      vec4.normalize(moveDir, moveDir);
 
-  if (isGrounded) {
-    this.gravityVelocity = 0;
+    let speed = this.isGrounded ? this.speed : this.airSpeed;
+    vec4.scale(moveDir, moveDir, speed * dt);
+    
+    let globalMatrix = mat4.create();
+    mat4.mul(globalMatrix, this.root.matrix, this.body.matrix);
+    
+    let moveVector = vec4.create();
+    vec4.transformMat4(moveVector, moveDir, globalMatrix);
+    moveVector = this.vec3FromVec4(moveVector);
+    // apply movement
+    vec3.add(this.root.translation, this.root.translation, moveVector);
+    
+    // apply gravity
+    let bodyDownDir = this.vec3FromVec4(vec4.transformMat4(vec4.create(), vec4.fromValues(0, -1, 0, 0), globalMatrix));
+    let globalBodyPos = this.vec3FromVec4(vec4.transformMat4(vec4.create(), vec4.fromValues(0, 0, 0, 1), globalMatrix));
+    let gravityDir = vec3.normalize(vec3.create(), globalBodyPos);
+    vec3.scale(gravityDir, gravityDir, -1);
 
-    if (this.keysDictionary['Space']) {
-      this.gravityVelocity = 10;
+    let rotation = quat.rotationTo(quat.create(), bodyDownDir, gravityDir);
+    quat.mul(this.root.rotation, rotation, this.root.rotation);
+    
+    let globalRootPos = this.vec3FromVec4(vec4.transformMat4(vec4.create(), vec4.fromValues(0, 0, 0, 1), this.root.matrix));
+    // TODO: this should be checked via collision detection
+    this.isGrounded = vec3.len(globalRootPos) <= 10;
+
+    if (this.isGrounded) {
+      this.gravityVelocity = 0;
+
+      if (this.keysDictionary['Space']) {
+        this.gravityVelocity = this.jumpHeight;
+      }
+    } else {
+      this.gravityVelocity += -this.gravityAcceleration * dt;
     }
-  } else {
-    this.gravityVelocity += -this.gravityAcceleration * dt;
+
+    let gravityMove = vec3.scale(vec3.create(), gravityDir, -this.gravityVelocity * dt);
+    vec3.add(this.root.translation, this.root.translation, gravityMove);
   }
 
-  let gravityMove = vec3.scale(vec3.create(), gravityDir, -this.gravityVelocity * dt);
-  vec3.add(this.root.translation, this.root.translation, gravityMove);
-}
-
-update(t, dt) {
-  this.updateRotation();
-  this.updateMovement(dt);
-  this.mouseMoving = false;
-}
-
-keydownHandler(e) {
-  this.keys[e.code] = true;
-}
-
-keyupHandler(e) {
-  this.keys[e.code] = false;
-}
-
+  update(t, dt) {
+    this.updateRotation();
+    this.updateMovement(dt);
+    this.mouseMoving = false;
+  }
 }
