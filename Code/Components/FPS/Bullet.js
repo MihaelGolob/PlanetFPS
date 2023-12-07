4c85553d96b891cb86681b0a8d00fca2b5bc4128
Nejc Rus <nejc@localhost.localdomain>
2023-12-07 23:07:43 +0100

    pew pew sounds

--- Code/Components/FPS/Bullet.js
+++ Code/Components/FPS/Bullet.js
@@ -7,44 +7,50 @@ import { Collider } from '../Collider.js'
 import { Node } from '../../../common/engine/core/Node.js';
 
 export class Bullet {
-    constructor(parent, startPosition, direction) {
-        // bullet parameters
-        this.speed = 50;
-        this.damage = 15;
-        this.lifetime = 3;
-        this.parent = parent;
-        this.startPosition = startPosition;
-        this.direction = direction;
-    } 
+  constructor(parent, startPosition, direction) {
+    // bullet parameters
+    this.speed = 50;
+    this.damage = 15;
+    this.lifetime = 3;
+    this.parent = parent;
+    this.startPosition = startPosition;
+    this.direction = direction;
+  }
 
-    async initialize() {
-        let loader = new GLTFLoader();
-        await loader.load('../Assets/Models/bullet.gltf');
-        this.bulletNode = loader.loadNode(0);
+  async initialize() {
+    let loader = new GLTFLoader();
+    await loader.load('../Assets/Models/bullet.gltf');
+    this.bulletNode = loader.loadNode(0);
 
-        // transform
-        this.transform = this.bulletNode.getComponentOfType(Transform);
-        this.transform.scale = [1, 1, 1];
-        this.transform.translation = this.startPosition;
-        
-        this.parent.addChild(this.bulletNode);
+    // transform
+    this.transform = this.bulletNode.getComponentOfType(Transform);
+    this.transform.scale = [1, 1, 1];
+    this.transform.translation = this.startPosition;
 
-        // movement
-        this.moveComponent = new MoveComponent(this.transform, this.speed, this.direction);
-        this.bulletNode.addComponent(this.moveComponent);
+    this.parent.addChild(this.bulletNode);
 
-        // collider
-        let colliderNode = new Node();
-        colliderNode.addComponent(new Transform());
-        let collider = new Collider(this.bulletNode, 0.1, false, (hitNode) => {
-            this.parent.removeChild(this.bulletNode);
-            console.log('bullet collision, id:', hitNode.id);
-        }, true);
-        colliderNode.addComponent(collider);
-        this.bulletNode.addChild(colliderNode);
+    // movement
+    this.moveComponent = new MoveComponent(this.transform, this.speed, this.direction);
+    this.bulletNode.addComponent(this.moveComponent);
 
-        // self destroy
-        let selfDestroy = new SelfDestroyComponent(this.bulletNode, this.parent, this.lifetime);
-        this.bulletNode.addComponent(selfDestroy);
-    }
+    // collider
+    let colliderNode = new Node();
+    colliderNode.addComponent(new Transform());
+    let collider = new Collider(this.bulletNode, 0.1, false, (hitNode) => {
+      this.parent.removeChild(this.bulletNode);
+      console.log('bullet collision, id:', hitNode.id);
+    }, true);
+    colliderNode.addComponent(collider);
+    this.bulletNode.addChild(colliderNode);
+
+    // self destroy
+    let selfDestroy = new SelfDestroyComponent(this.bulletNode, this.parent, this.lifetime);
+    this.bulletNode.addComponent(selfDestroy);
+
+
+    // play sound
+    const shootSound = document.getElementById('shoot-sound');
+    shootSound.currentTime = 0;
+    shootSound.play();
+  }
 }
