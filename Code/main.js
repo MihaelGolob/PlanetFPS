6ef302e8600cee5cba7a06b7ceb8994ef6e41ba8
Nejc Rus <nejcrus7@gmail.com>
2023-12-10 19:26:46 +0100

    removed pointer lock from main

--- Code/main.js
+++ Code/main.js
@@ -7,38 +7,6 @@ let oldTime = Date.now();
 
 const canvas = document.querySelector('canvas');
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOM fully loaded and parsed');
-  const canvas = document.getElementById('canvas');
-
-  // Function to lock the pointer
-  function lockPointer() {
-    canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
-    canvas.requestPointerLock();
-  }
-
-  // Event listener for canvas click
-  canvas.addEventListener('click', () => {
-    // if (/* condition to check if pointer should be locked, e.g., not in game menu */) {
-    lockPointer();
-    // }
-  });
-
-  // Event listener for pointer lock change
-  document.addEventListener('pointerlockchange', lockPointerChange, false);
-  document.addEventListener('mozpointerlockchange', lockPointerChange, false);
-
-  function lockPointerChange() {
-    if (document.pointerLockElement === canvas || document.mozPointerLockElement === canvas) {
-      console.log('The pointer lock status is now locked');
-      // Add your logic for when the cursor is locked
-    } else {
-      console.log('The pointer lock status is now unlocked');
-      // Add your logic for when the cursor is unlocked
-    }
-  }
-});
-
 // play background mucic
 const backgroundMusic = document.getElementById('background-music');
 backgroundMusic.volume = 0.1;
